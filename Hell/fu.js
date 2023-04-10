87c902b386afeb7ff8f6a80e7a32e0ef002630bd
FelipeArnus <79066040+FelipeArnus@users.noreply.github.com>
2023-04-10 11:44:56 -0300

    Add files via upload

---
+++ Hell/fu.js
@@ -0,0 +1,26 @@
+    let x = 0;
+
+    
+    document.write("<br>" + "Oque queremos?" + "<br>");
+    document.body.style.backgroundColor = "#AA0000";
+
+    do{
+
+    document.write("Queijo quente" + "<br>");
+    document.write("<br>" + "batidinha" + "<br>" + "<br>");
+    x = x + 1;
+
+    }while(x < 3)
+
+    document.write("<br>")
+
+//---------------------------------------------------------------------------
+    
+    function humni(){
+
+        let = y.getElementById("idGenerico")//pega elemento do html pelo id, o atribuindo a y
+
+        document.getElementById("humni").innerHTML = novo.value
+        //pega valor enviado pelo botão no HTML, o atruibuindo um valor
+
+    }
